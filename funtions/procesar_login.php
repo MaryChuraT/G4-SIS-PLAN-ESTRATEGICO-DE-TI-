38067d7dcab2eafc934076b8b5f94642f7302327
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-11 18:08:40 -0500

    Update procesar_login.php
    #1

--- funtions/procesar_login.php
+++ funtions/procesar_login.php
@@ -1,4 +1,15 @@
 <?php
 session_start();
 require '../config/conexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT id, password FROM usuarios WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+
+}
 ?>
