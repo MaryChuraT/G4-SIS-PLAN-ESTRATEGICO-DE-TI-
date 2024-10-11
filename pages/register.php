7c9128ce48eea1bafa1da1c9c8f33aeb60a4e394
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-11 18:17:15 -0500

    Update register.php
    #1

---
+++ pages/register.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro de Usuario</title>
+    <link rel="stylesheet" href="../assets/styles.css">
+</head>
+
+</html>
