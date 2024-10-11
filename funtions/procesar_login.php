8086bb529784c4a7c05184965d0f1074c270caec
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-11 18:09:08 -0500

    Update procesar_login.php
    #1

--- funtions/procesar_login.php
+++ funtions/procesar_login.php
@@ -11,5 +11,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->execute();
     $stmt->store_result();
 
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $hash_password);
+        $stmt->fetch();
+        
+        if (password_verify($password, $hash_password)) {
+            $_SESSION['username'] = $username;
+            header("Location: ../pages/bienvenida.php");
+            exit();
+        } else {
+            header("Location: ../pages/login.php?mensaje=Contraseña incorrecta.");
+            exit();
+        }
+    }
 }
 ?>
