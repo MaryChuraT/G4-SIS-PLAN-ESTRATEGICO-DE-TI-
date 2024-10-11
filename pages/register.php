2bd0b8eae200467758f1622f296d37a5c65e5229
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-11 18:17:27 -0500

    Update register.php
    #1

--- pages/register.php
+++ pages/register.php
@@ -5,5 +5,19 @@
     <title>Registro de Usuario</title>
     <link rel="stylesheet" href="../assets/styles.css">
 </head>
+<body>
+    <div class="container">
+        <h2>Registrar Usuario</h2>
+        <form action="../funtions/procesar_register.php" method="post">
+            <label for="username">Usuario:</label>
+            <input type="text" id="username" name="username" required>
 
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required>
+
+            <input type="submit" value="Registrar">
+            <button type="button" onclick="window.location.href='login.php'">Regresar</button>
+        </form>
+    </div>
+</body>
 </html>
