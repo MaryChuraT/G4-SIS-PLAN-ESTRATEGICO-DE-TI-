960888fc04f45d40f67d5eafa1306f5bfebe73a8
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-11 18:16:09 -0500

    Update procesar_register.php
    #1

--- funtions/procesar_register.php
+++ funtions/procesar_register.php
@@ -11,6 +11,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($stmt->num_rows > 0) {
         header("Location: ../pages/register.php?mensaje=El usuario ya existe.");
+    } else {
+        $hash_password = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $conn->prepare("INSERT INTO usuarios (username, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $username, $hash_password);
+
+        if ($stmt->execute()) {
+            header("Location: ../pages/login.php?mensaje=Usuario registrado con éxito.");
+        } else {
+            header("Location: ../pages/register.php?mensaje=Error al registrar el usuario.");
+        }
     }
 }
 ?>
