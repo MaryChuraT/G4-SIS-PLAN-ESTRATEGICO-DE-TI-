8dea7d59c58ef14b18be262b917af1eaa7d15030
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-11 18:15:30 -0500

    Update procesar_register.php
    #1

--- funtions/procesar_register.php
+++ funtions/procesar_register.php
@@ -3,5 +3,10 @@ require '../config/conexion.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT id FROM usuarios WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
 }
 ?>
