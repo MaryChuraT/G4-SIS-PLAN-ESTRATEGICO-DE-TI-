7b5dac1fc36abc172503e63c89e94a1e9a173ef4
MrPol4r <102675629+MrPol4r@users.noreply.github.com>
2024-10-18 16:27:35 -0500

    Update procesar_login.php
    #8

--- funtions/procesar_login.php
+++ funtions/procesar_login.php
@@ -6,29 +6,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT id, password FROM usuarios WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $stmt->store_result();
+    // Conectarse a la base de datos utilizando la función de conexion.php
+    $conn = conectarse();
 
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $hash_password);
-        $stmt->fetch();
-        
-        if (password_verify($password, $hash_password)) {
-            $_SESSION['username'] = $username;
-            header("Location: ../pages/bienvenida.php");
-            exit();
+    if ($conn) {
+        // Preparar la consulta SQL para evitar inyecciones
+        $stmt = $conn->prepare("SELECT id, password FROM usuarios WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($id, $hash_password);
+            $stmt->fetch();
+
+            // Verificar la contraseña
+            if (password_verify($password, $hash_password)) {
+                // Si la contraseña es correcta, iniciar sesión
+                $_SESSION['username'] = $username;
+                header("Location: ../pages/index.php");
+                exit();
+            } else {
+                // Si la contraseña es incorrecta
+                header("Location: ../pages/login.php?mensaje=Contraseña incorrecta.");
+                exit();
+            }
         } else {
-            header("Location: ../pages/login.php?mensaje=Contraseña incorrecta.");
+            // Si el usuario no existe
+            header("Location: ../pages/login.php?mensaje=Usuario no encontrado.");
             exit();
         }
+
+        // Cerrar la sentencia y la conexión
+        $stmt->close();
+        $conn->close();
     } else {
-        header("Location: ../pages/login.php?mensaje=Usuario no encontrado.");
+        // Si la conexión falla
+        echo "Error conectando a la Base de Datos.";
         exit();
     }
-
-    $stmt->close();
-    $conn->close();
 }
 ?>
